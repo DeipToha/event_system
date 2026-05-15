4b9618c28ec2dc847f220a15d938ad49818257f3
DeipToha <rakibulislamdeep@gmail.com>
2026-05-15 14:28:26 +0600

    demo event

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 session_start();
 
+include("config/db.php");
+
 if(!isset($_SESSION['user_id']))
 {
     header("Location: login.php");
 }
+
+$sql = "
+SELECT events.*, categories.name AS category_name
+FROM events
+LEFT JOIN categories
+ON events.category_id = categories.id
+WHERE status='published'
+ORDER BY event_datetime ASC
+";
+
+$result = mysqli_query($conn, $sql);
 
 ?>
 
@@ -13,23 +26,63 @@ if(!isset($_SESSION['user_id']))
 <html>
 
 <head>
-    <title>Dashboard</title>
+    <title>All Events</title>
 </head>
 
 <body>
 
-<h1>
-    Welcome <?php echo $_SESSION['user_name']; ?>
-</h1>
+<h1>Upcoming Events</h1>
 
 <p>
-    You are logged in as:
-    <?php echo $_SESSION['user_role']; ?>
+Welcome,
+<?php echo $_SESSION['user_name']; ?>
 </p>
 
-<a href="logout.php">
-    Logout
-</a>
+<a href="logout.php">Logout</a>
+
+<hr>
+
+<?php
+
+while($event = mysqli_fetch_assoc($result))
+{
+?>
+
+<div style="border:1px solid black; padding:15px; margin-bottom:20px;">
+
+    <h2>
+        <?php echo $event['title']; ?>
+    </h2>
+
+    <p>
+        <strong>Category:</strong>
+        <?php echo $event['category_name']; ?>
+    </p>
+
+    <p>
+        <strong>City:</strong>
+        <?php echo $event['city']; ?>
+    </p>
+
+    <p>
+        <strong>Venue:</strong>
+        <?php echo $event['venue_name_override']; ?>
+    </p>
+
+    <p>
+        <strong>Date:</strong>
+        <?php echo $event['event_datetime']; ?>
+    </p>
+
+    <p>
+        <?php echo $event['description']; ?>
+    </p>
+
+</div>
+
+<?php
+}
+?>
 
 </body>
 </html>
