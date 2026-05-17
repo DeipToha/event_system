2f90800ba47e36e6e52dce5629c78b0492c0ae0e
Sudipta Sarker Unnayan <22-47656-2@student.aiub.edu>
2026-05-17 19:55:41 +0600

    updates on event controller editing form

--- organiser/views/event/edit.php
+++ organiser/views/event/edit.php
@@ -4,11 +4,13 @@
 <div class="card">
     <?php if ($error): ?><div class="alert alert-error"><?= htmlspecialchars($error) ?></div><?php endif; ?>
 
-    <form method="POST" action="index.php?page=events&action=edit&id=<?= $event['id'] ?>" enctype="multipart/form-data">
+    <form method="POST" action="index.php?page=events&action=edit&id=<?= $event['id'] ?>" enctype="multipart/form-data" id="event-form">
+        <?= csrfField() ?>
         <div class="form-grid mb-2">
             <div class="form-group form-full">
                 <label class="form-label">Event Title *</label>
-                <input type="text" name="title" class="form-control" value="<?= htmlspecialchars($event['title']) ?>" required>
+                <input type="text" name="title" id="title" class="form-control" value="<?= htmlspecialchars($event['title']) ?>">
+                <span id="err-title" style="color:var(--error,#e53e3e);font-size:12px;"></span>
             </div>
             <div class="form-group form-full">
                 <label class="form-label">Description</label>
@@ -32,11 +34,13 @@
             </div>
             <div class="form-group">
                 <label class="form-label">Event Start Date & Time *</label>
-                <input type="datetime-local" name="event_datetime" class="form-control" value="<?= date('Y-m-d\TH:i', strtotime($event['event_datetime'])) ?>" required>
+                <input type="datetime-local" name="event_datetime" id="event_datetime" class="form-control" value="<?= date('Y-m-d\TH:i', strtotime($event['event_datetime'])) ?>">
+                <span id="err-event_datetime" style="color:var(--error,#e53e3e);font-size:12px;"></span>
             </div>
             <div class="form-group">
                 <label class="form-label">Event End Date & Time *</label>
-                <input type="datetime-local" name="end_datetime" class="form-control" value="<?= date('Y-m-d\TH:i', strtotime($event['end_datetime'])) ?>" required>
+                <input type="datetime-local" name="end_datetime" id="end_datetime" class="form-control" value="<?= date('Y-m-d\TH:i', strtotime($event['end_datetime'])) ?>">
+                <span id="err-end_datetime" style="color:var(--error,#e53e3e);font-size:12px;"></span>
             </div>
         </div>
 
